0851efc2f96c7f43b45898947ba9aaf917a9154e
Patrick Göler von Ravensburg <mail@pgvr.dev>
2020-01-30 16:49:29 +0100

    change header logo to svg

--- src/components/Title.svelte
+++ src/components/Title.svelte
@@ -7,9 +7,10 @@
     @apply uppercase text-center text-2xl mb-12 font-bold;
   } */
   img {
+    width: 500px;
     @apply mx-auto mb-12;
   }
 </style>
 
 <!-- <h1>Hustle Timer</h1> -->
-<img src="logo_small.png" alt="title" />
+<img src="logo.svg" alt="title" />
